c4ec7646665e59e487f18c473d62795bf7f94f61
Jime Matthies <JimeMatthies@gmail.com>
2022-09-07 16:20:57 -0300

    Primera Actualizacion

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("JM was Here!");
 };
+
+const generateDeck = () => {
+  let deck = [];
+  let suits = "♦♥♠♣";
+  let number = "A123456789JQK";
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < number.length; j++) {
+      let card = "";
+      card += suits[i] + number[j];
+      deck.push(card);
+    }
+  }
+  return deck;
+};
+
+const getCard = () => {
+  let cardList = generateDeck();
+  let random = Math.round(Math.random() * 52);
+
+  return cardList[random];
+};
+
+console.log(generateDeck());
+console.log(getCard());
+
+document.getElementById("draw-btn").addEventListener("click", addCard);
+
+function addCard() {
+  if (usernumber.value === "1") {
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+    let newCardNumber = document.createTextNode(getCard());
+    newCard.appendChild(newCardNumber);
+    return document.querySelector("#cardfrominput").appendChild(newCard);
+  } else {
+    console.log("need number from user");
+  }
+}
