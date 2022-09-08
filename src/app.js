0df804baa608d02afee3259848f080d24b74c1e4
Jime Matthies <JimeMatthies@gmail.com>
2022-09-08 17:31:48 -0300

    getCard OK

--- src/app.js
+++ src/app.js
@@ -6,34 +6,35 @@ window.onload = function() {
   console.log("JM was Here!");
 };
 
+let newDeck = [];
+const suits = ["♣", "♦", "♥", "♠"];
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let cardSection = document.querySelector("#cardsfrominput");
+
 document.getElementById("draw-btn").addEventListener("click", getCards);
 
 function getCards() {
-  const suits = ["♣", "♦", "♥", "♠"];
-  const ranks = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-
-  let newDeck = [];
-  let cardSection = document.querySelector("#cardsfrominput");
+  newDeck = [];
+  cardSection.innerHTML = "";
 
   for (let i = 0; i < inputnumber.value; i++) {
-    let randomsuit = suits[Math.floor(Math.random() * 4)];
-    let randomrank = ranks[Math.floor(Math.random() * 13)];
-
-    //let color = randomsuit === "♥" || randomsuit === "♦" ? "red" : "black";
+    let randomsuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomrank = ranks[Math.floor(Math.random() * ranks.length)];
 
     let objCard = {};
     objCard.suit = randomsuit;
@@ -41,25 +42,31 @@ function getCards() {
     console.log(objCard);
     newDeck.push(objCard);
 
-    let suitsObtained = newDeck.map(item => item.suit);
-    console.log(suitsObtained);
-    let ranksObtained = newDeck.map(item => item.rank);
-    console.log(ranksObtained);
-
     let divCard = document.createElement("div");
     let divRank = document.createElement("div");
     let divSuitTop = document.createElement("div");
     let divSuitBot = document.createElement("div");
 
     divCard.classList.add("card");
+    divRank.classList.add("rank");
+    divSuitTop.classList.add("suit-top");
+    divSuitBot.classList.add("suit-bot");
 
-    divRank.innerHTML = ranksObtained;
-    divSuitTop.innerHTML = suitsObtained;
-    divSuitBot.innerHTML = suitsObtained;
+    divRank.style.color =
+      randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+    divSuitTop.style.color =
+      randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+    divSuitBot.style.color =
+      randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+
+    divRank.innerHTML = objCard.rank;
+    divSuitTop.innerHTML = objCard.suit;
+    divSuitBot.innerHTML = objCard.suit;
 
     cardSection.append(divCard);
     divCard.append(divSuitTop, divRank, divSuitBot);
-    newDeck = [];
-    console.log(newDeck);
   }
+  console.log(newDeck);
 }
+
+console.log(newDeck);
