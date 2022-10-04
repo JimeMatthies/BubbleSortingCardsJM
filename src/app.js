70d91c59472d4d1b026b1c5e4a21bbfc08b26056
Jime Matthies <JimeMatthies@gmail.com>
2022-10-04 14:31:26 -0300

    Bubble Sorting Cards JM

--- src/app.js
+++ src/app.js
@@ -2,14 +2,9 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  console.log("JM was Here!");
-};
-
-let newDeck = [];
 const suits = ["♣", "♦", "♥", "♠"];
 const ranks = [
-  "A",
+  "1",
   "2",
   "3",
   "4",
@@ -19,23 +14,68 @@ const ranks = [
   "8",
   "9",
   "10",
-  "J",
-  "Q",
-  "K"
+  "11",
+  "12",
+  "13"
 ];
+const cardSection = document.querySelector("#inputcards");
+const sortSection = document.querySelector("#sortcards");
+const drawbtn = document.querySelector("#draw-btn");
+const sortbtn = document.querySelector("#sort-btn");
+const input = document.querySelector("#inputnumber");
 
-let cardSection = document.querySelector("#cardsfrominput");
-let sortSection = document.querySelector("#firstsort");
-let drawbtn = document.querySelector("#draw-btn");
-let sortbtn = document.querySelector("#sort-btn");
+window.onload = function() {
+  console.log("JM was Here!");
 
-drawbtn.addEventListener("click", getCards);
-sortbtn.addEventListener("click", () => {
-  //sortCards(newDeck);
-  console.log(bubbleSort(newDeck, "value"));
-});
+  let newDeck = [];
 
-function createCard(objCard, section, randomsuit, randomrank) {
+  drawbtn.addEventListener("click", event => {
+    newDeck = getCards(event);
+    sortSection.innerHTML = "";
+  });
+
+  sortbtn.addEventListener("click", event => {
+    sortSection.innerHTML = "";
+    bubbleSort(newDeck);
+  });
+};
+
+function getCards(event) {
+  event.preventDefault();
+
+  let deck = [];
+  cardSection.innerHTML = "";
+
+  for (let i = 0; i < input.value; i++) {
+    let randomsuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomrank = ranks[Math.floor(Math.random() * ranks.length)];
+
+    let objCard = {};
+    objCard.suit = randomsuit;
+    objCard.rank = randomrank;
+    objCard.index = objCard.rank;
+
+    if (objCard.rank == 1) {
+      objCard.rank = "A";
+    }
+    if (objCard.rank == 11) {
+      objCard.rank = "J";
+    }
+    if (objCard.rank == 12) {
+      objCard.rank = "Q";
+    }
+    if (objCard.rank == 13) {
+      objCard.rank = "K";
+    }
+
+    deck.push(objCard);
+
+    paintCard(objCard, cardSection);
+  }
+  return deck;
+}
+
+function paintCard(objCard, section) {
   let divCard = document.createElement("div");
   let divRank = document.createElement("div");
   let divSuitTop = document.createElement("div");
@@ -44,14 +84,13 @@ function createCard(objCard, section, randomsuit, randomrank) {
   divRank.innerHTML = objCard.rank;
   divSuitTop.innerHTML = objCard.suit;
   divSuitBot.innerHTML = objCard.suit;
-  divRank.innerHTML = objCard.value;
 
   divRank.style.color =
-    randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+    objCard.suit === "♦" || objCard.suit === "♥" ? "red" : "black";
   divSuitTop.style.color =
-    randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+    objCard.suit === "♦" || objCard.suit === "♥" ? "red" : "black";
   divSuitBot.style.color =
-    randomsuit === "♦" || randomsuit === "♥" ? "red" : "black";
+    objCard.suit === "♦" || objCard.suit === "♥" ? "red" : "black";
 
   divCard.classList.add("card");
   divRank.classList.add("rank");
@@ -62,70 +101,22 @@ function createCard(objCard, section, randomsuit, randomrank) {
   divCard.append(divSuitTop, divRank, divSuitBot);
 }
 
-function getCards() {
-  newDeck = [];
-  cardSection.innerHTML = "";
-
-  for (let i = 0; i < inputnumber.value; i++) {
-    let randomsuit = suits[Math.floor(Math.random() * suits.length)];
-    let randomrank = ranks[Math.floor(Math.random() * ranks.length)];
-
-    let objCard = {};
-    objCard.suit = randomsuit;
-    objCard.rank = randomrank;
-    objCard.value = objCard.rank;
-
-    console.log(objCard);
-    newDeck.push(objCard);
-
-    createCard(objCard, cardSection, randomsuit, randomrank);
-  }
-  console.log(newDeck);
-}
-
-function bubbleSort(a, par) {
-  var swapped;
-  do {
-    swapped = false;
-    for (var i = 0; i < a.length - 1; i++) {
-      if (a[i][par] > a[i + 1][par]) {
-        var temp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = temp;
-        swapped = true;
+function bubbleSort(newDeck) {
+  let size = newDeck.length;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size - 1; j++) {
+      if (parseInt(newDeck[j].index) > parseInt(newDeck[j + 1].index)) {
+        let temp = newDeck[j];
+        newDeck[j] = newDeck[j + 1];
+        newDeck[j + 1] = temp;
       }
     }
-  } while (swapped);
-}
-
-//console.log(bubbleSort(newDeck, "value"));
-
-/* const bubbleSort = (arr = []) => {
-  let wall = arr.length; // 10
-  for (let i = 0; i < wall; i++) {
-    for (let j = 0; j < wall - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+    let sortRow = document.createElement("div");
+    sortRow.classList.add("col-md-12", "d-flex", "justify-content-center");
+    sortSection.appendChild(sortRow);
+    for (let i = 0; i < newDeck.length; i++) {
+      paintCard(newDeck[i], sortRow);
     }
   }
-  return arr;
-}; */
-
-/* function sortCards(array) {
-  let sortDeck = [];
-  sortSection.innerHTML = " ";
-  array.map(({ value }) => {
-    return sortDeck.push([value]);
-  });
-
-  bubbleSort(sortDeck);
-  console.log(array);
-
-  for (let i = 0; i < sortDeck.length; i++) {
-    createCard(sortDeck[i], sortSection, array[i].suit, array[i].rank);
-  }
-  console.log(sortDeck);
-} */
+  return newDeck;
+}
